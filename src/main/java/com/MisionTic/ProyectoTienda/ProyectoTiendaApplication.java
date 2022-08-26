0475383eae66d2372fb884db1f7aa7f79be3e96f
Edwin Alberto Zuluaga Sanchez <edwinzuluaga@gmail.com>
2022-08-25 20:08:29 -0500

    Subiendo Proyecto

---
+++ src/main/java/com/MisionTic/ProyectoTienda/ProyectoTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.MisionTic.ProyectoTienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTiendaApplication.class, args);
+	}
+
+}
