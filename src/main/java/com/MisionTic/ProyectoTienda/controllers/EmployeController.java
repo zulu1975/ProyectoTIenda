0376fe0cddcbe6471a7dd087e9149046a8db2dd3
Edwin Alberto Zuluaga Sanchez <edwinzuluaga@gmail.com>
2022-09-09 21:20:01 -0500

    Subiendo CRUD Empleado

--- src/main/java/com/MisionTic/ProyectoTienda/controllers/EmployeController.java
+++ src/main/java/com/MisionTic/ProyectoTienda/controllers/EmployeController.java
@@ -3,20 +3,18 @@ package com.MisionTic.ProyectoTienda.controllers;
 import com.MisionTic.ProyectoTienda.Interfaces.IEmployeService;
 import com.MisionTic.ProyectoTienda.entities.Employe;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
 
 @Controller
-@RequestMapping
+@RequestMapping("employe")
 public class EmployeController {
-
+    //Aqui estamos inyectando las dependencias hacia el servicio
     @Autowired
     private IEmployeService employeService;
 
@@ -31,14 +29,29 @@ public class EmployeController {
     @GetMapping("/create")
     public String createEmploye(Model model){
         Employe employe = new Employe();
+        model.addAttribute("titulo", "Crear Empleado");
         model.addAttribute("employe", employe);
         return "views/employe/createEmploye";
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute Employe employe){
+    public String saveEmploye(@ModelAttribute Employe employe){
         employeService.save(employe);
         return "redirect:/views/employe/";
     }
 
+    @GetMapping("/update/{id}")
+    public String updateEmploye(@PathVariable("id") Long idEmploye, Model model){
+        Employe employe = employeService.searchById(idEmploye);
+        model.addAttribute("titulo", "Actualizar Empleado");
+        model.addAttribute("employe", employe);
+        return "views/employe/createEmploye";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEmploye(@PathVariable("id") Long idEmploye){
+        employeService.delete(idEmploye);
+        System.out.println("Registro eliminado con exito");
+        return "redirect:/views/employe/";
+    }
 }
--- src/main/java/com/MisionTic/ProyectoTienda/entities/Employe.java
+++ src/main/java/com/MisionTic/ProyectoTienda/entities/Employe.java
@@ -1,52 +1,76 @@
 package com.MisionTic.ProyectoTienda.entities;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.context.annotation.Profile;
+
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity
+@Table("employe")
 public class Employe {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="employe_id")
     private long id;
-    @Column(name = "email")
     private String email;
-
-    @Column(name = "CreatedAt")
+    @Column(name = "created_at")
     private LocalDate createdAt;
-    @Column (name = "UpdatedAt")
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name=profile_id)
+    private Profile profile;
+
+    private Enum_RoleName role;
+
+    @Column (name = "updated_at")
     private LocalDate updateAt;
+
+    @ManyToOne
+    @JoinColumn(name=enterorise_id)
+    private Enterprise enterprise;
+
+    @OneToMany(mappedBy = "employe")
+    private List<Trasaction> transaction;
+
 
     public Employe(){
     }
+
     public Employe(long id, String email, LocalDate createdAt, LocalDate updateAt) {
         this.id = id;
         this.email = email;
         this.createdAt = createdAt;
         this.updateAt = updateAt;
     }
+
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public LocalDate getCreatedAt() {
         return createdAt;
     }
+
     public void setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
     }
+
     public LocalDate getUpdateAt() {
         return updateAt;
     }
+
     public void setUpdateAt(LocalDate updateAt) {
         this.updateAt = updateAt;
     }
